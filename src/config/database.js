dc605d29d66f230bf626a436422cb31fbe3b1fd6
Mauricio1maciel <macielmauriciio@gmail.com>
2026-06-22 20:39:06 -0300

    fix: permite que a placa do carro fique vazia para visitantes e moradores

--- src/config/database.js
+++ src/config/database.js
@@ -1,19 +1,19 @@
 import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-    define: {
-      timestamps: true,
-      underscored: true
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialect: 'postgres',
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false // Fundamental para o Supabase aceitar a conexão
     }
+  },
+  define: {
+    timestamps: true,
+    underscored: true
   }
-);
+});
 
 export default sequelize;
